84a4b8ee6e80905b5bf4c6ed9a1f7c98bb5dd00b
nomayen <nomayen.ohin@gmail.com>
2023-01-02 01:47:45 +0600

    one

--- src/App.js
+++ src/App.js
@@ -25,7 +25,12 @@ function App() {
       body: JSON.stringify(user)
     })
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        console.log(data);
+        const newUsers =[...users, data];
+        setUsers(newUsers)
+
+      })
       .catch(err => console.error(err))
     event.target.reset();
   }
